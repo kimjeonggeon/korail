9c3c558ebccb07e2680e7a62101236d8a8d44aa4
Junwoo Lim <dlawnsdn1209@naver.com>
2023-07-18 11:36:10 +0900

    좌석 페이지 + 오더 dto수정

--- src/main/java/com/example/korail/dto/OrderDto.java
+++ src/main/java/com/example/korail/dto/OrderDto.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class OrderDto {
-    private String price,sstation, rdate, stime, dtime, dstation, reservnum, chairnum, id, depPlaceId, arrPlaceId, depPlandTime, cardnum, cdate,email;
-    private int rno, trainnum, ticketqty, cancel;
+    private String sstation, rdate, stime, dtime, dstation, reservnum, chairnum, id, depPlaceId, arrPlaceId, depPlandTime, cardnum, cdate,email;
+    private int rno, trainnum, ticketqty, cancel,price;
 }
