41562d4da1b618431249219ed86b7c4055222d5f
wunwoolim <dlawnsdn1209@naver.com>
2023-08-07 18:04:47 +0900

    board crud 함수화 연습 성공

--- src/main/java/com/example/korail/service/BoardService.java
+++ src/main/java/com/example/korail/service/BoardService.java
@@ -13,6 +13,25 @@ public class BoardService {
     @Autowired
     BoardMapper boardMapper;
 
+    public int delete(String bid){
+        System.out.println("서비스는 와지나?");
+        return boardMapper.delete(bid);
+    }
+    public int update(BoardDto boardDto){
+
+        return boardMapper.update(boardDto);
+    }
+    public BoardDto boardupdate(String bid){
+
+        return boardMapper.boardupdate(bid);
+    }
+    public BoardDto content(String bid){
+
+        return boardMapper.content(bid);
+    }
+    public int getRegistration(BoardDto boardDto){
+        return boardMapper.registration(boardDto);
+    }
     public List<BoardDto> list(PageDto pageDto){
         return boardMapper.list(pageDto);
     }
